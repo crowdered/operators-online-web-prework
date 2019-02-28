6f46206d741217b25ea04d5236e7e44b5c83e159
Mark Crowder <crowdered@gmail.com>
2019-02-28 00:48:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,11 @@ end
 	
 def not_safe?(speed)
 
-  if speed > 60 ? true : false
-  elsif speed < 40 ? true : false
-  elsif speed > 40 && speed < 60 ? true : false
-end
+  if speed > 60
+    true
+  elsif speed < 40
+    true
+  elsif speed > 40 && speed < 60
+    true
+  end
 end
